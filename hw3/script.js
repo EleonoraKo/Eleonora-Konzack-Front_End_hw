dc2a66379505d6a9d5c4cc6483e71f9e201ef461
Eleonora Konzack <elyamd@mail.ru>
2023-03-17 22:48:40 +0100

    hw3

--- hw3/script.js
+++ hw3/script.js
@@ -2,10 +2,10 @@
 let firstNumber = +prompt("Write a number 1?");
 let secondtNumber = +prompt("Write a number 2?");
 
-alert(`${firstNumber} + ${secondtNumber} = ${firstNumber+secondtNumber}
-${firstNumber} - ${secondtNumber} = ${firstNumber-secondtNumber}
-${firstNumber} * ${secondtNumber} = ${firstNumber*secondtNumber}
-${firstNumber} / ${secondtNumber} = ${firstNumber/secondtNumber}`)
+alert(`· ${firstNumber} + ${secondtNumber} = ${firstNumber+secondtNumber}
+· ${firstNumber} - ${secondtNumber} = ${firstNumber-secondtNumber}
+· ${firstNumber} * ${secondtNumber} = ${firstNumber*secondtNumber}
+· ${firstNumber} / ${secondtNumber} = ${firstNumber/secondtNumber}`)
 
 
 
