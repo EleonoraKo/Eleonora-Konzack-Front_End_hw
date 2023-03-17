efd0529a6087a1b6b3b1035f8434d602fa50495b
Eleonora Konzack <elyamd@mail.ru>
2023-03-17 22:32:32 +0100

    hw5

---
+++ hw5/script.js
@@ -0,0 +1,5 @@
+let firstNumber = +prompt("Write a number 1?");
+let secondtNumber = +prompt("Write a number 2?");
+let thirdNumber = +prompt("Write a number 3?");
+
+alert("the average number is " +(firstNumber + secondtNumber + thirdNumber) / 3)
