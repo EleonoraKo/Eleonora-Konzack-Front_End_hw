afe9a61cbba880c77e6a86f5d6159498e7239198
Eleonora Konzack <elyamd@mail.ru>
2023-04-13 13:47:06 +0200

    hw18

---
+++ hw18/script.js
@@ -0,0 +1,15 @@
+// Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// ﻿sum(3) = 3
+// sum(5) = 8
+// sum(20) = 28
+
+let sum = (function () {
+    let result = 0;
+    return function(a) {
+    result += a;
+    return result; }
+})();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20)); 
