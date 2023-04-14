31025e5520b5024ad5084f5205ecac07a01c19f4
Eleonora Konzack <elyamd@mail.ru>
2023-04-14 21:50:43 +0200

    hw7

--- hw7/script.js
+++ hw7/script.js
@@ -1,20 +1,19 @@
+
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
-
-
-
-switch(numOrStr){
-    case null:
-    console.log('ви скасували');
-    break;
-    case '':       
-    console.log('Empty String');
-    break;
-    case 'NaN':       
-    console.log(' number is Ba_NaN');
-    break;
-    default:       
-    console.log('OK!');
+switch(numOrStr) {
+	case null:
+  console.log('ви скасували');
+  break;
+  case '':
+  console.log('Empty String');
+  break;
+  case !isNaN(numOrStr) || numOrStr :
+  console.log('number is Ba_NaN');
+  break;
+  default:
+  console.log('OK!');
+  break;
 }
 
 // if(numOrStr === null) {
