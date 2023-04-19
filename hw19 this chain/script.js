795e8116694d10245722652eb198bc81e64e6ef9
Eleonora Konzack <elyamd@mail.ru>
2023-04-19 13:32:53 +0200

    hw19

---
+++ hw19 this chain/script.js
@@ -0,0 +1,32 @@
+// Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+// let ladder = {
+//     step: 0,
+//     up: function () {
+//         this.step++;
+//     },
+//     down: function () {
+//         this.step--;
+//     },
+//     showStep: function () { // показывает текущую ступеньку
+//         alert(this.step);
+//     }
+// };
+
+let ladder = {
+        step: 0,
+        up: function () {
+            this.step++;
+            return this;
+        },
+        down: function () {
+            this.step--;
+            return this;
+        },
+        showStep: function () {
+            alert(this.step);
+            return this;
+        }
+    };
+    ladder.up().up().down().showStep();
+
+   
--- hw20/script.js
+++ hw20/script.js
@@ -1,1 +1,21 @@
+function hello(){
+    console.log('hello', this)
+}
+hello();
+let user = {
+    name: 'Elya',
+    city: 'Kharkiv',
+    sayHello: hello,
+};
+user.sayHello();
 
+
+let car = {
+    brand: 'Audi',
+    model: 'A6',
+    year: 2021,
+    startEngine: function(){
+        console.log('Engine started')
+    }
+}
+car.startEngine();
