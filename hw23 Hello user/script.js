87e8e28acd54ca5044968884f86278b623b351c2
Eleonora Konzack <105788155+EleonoraKo@users.noreply.github.com>
2023-04-24 16:04:07 +0200

    Update script.js

--- hw23 Hello user/script.js
+++ hw23 Hello user/script.js
@@ -4,4 +4,4 @@
 // За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
 
 let name = prompt('What is your name?');
-alert(`Hello, ${name} ! How are you?`);
+alert(`Hello, ${name}! How are you?`);
