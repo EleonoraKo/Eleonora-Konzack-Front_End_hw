e5424e5e948cfe1165c950b477b1ae377e1c6a27
Eleonora Konzack <elyamd@mail.ru>
2023-05-22 17:28:52 +0200

    hw22

--- hw22 Студент/script.js
+++ hw22 Студент/script.js
@@ -1,24 +1,139 @@
 // Вам потрібно зробити конструктор сутності "Студент".
 // Студент має ім'я, прізвище, рік народження — це властивості. Є масив з оцінками, це також властивість. І є можливість отримати вік студента та його середній бал – це методи.
-class Student{
-    constructor(name, age, year){
-        this.name = name;
-        this.age = age;
+// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, але на 25 елементів. Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на чергове порожнє місце, в масив записується true, коли викликаємо .absent() - записується false. Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів. Масив – це властивість, present та absent – ​​методи.
+// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
+// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) і показати використання цих методів.
+
+class Student {
+    constructor(firstName, lastName, year, scores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.year = year;
-        this.marks = marks;
-        this.averageMark = [];
-        let Attendancy = new Array(25)
+        this.scores = scores;
     }
-    
+
+    attendancy = [];
+
+    getAge() {
+        return new Date().getFullYear() - this.year;
+    }
+
+    getAverageScore() {
+        let sum = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+            sum += this.scores[i];
+        }
+        return sum / this.scores.length;
+    }
+
+    present() {
+        if (this.attendancy.length <= 25) {
+            this.attendancy.push(true);
+        }
+    }
+    absent() {
+        if (this.attendancy.length <= 25) {
+            this.attendancy.push(false);
+        }
+    }
+
+    getVisetedClasses() {
+        let sumVisits = 0;
+        for (let i = 0; i < this.attendancy.length; i++) {
+            if (this.attendancy[i] === true) {
+                sumVisits++;
+            }
+        }
+        return sumVisits;
+    }
+
+    summary() {
+        const sumVisits = this.getVisetedClasses();
+        if ((sumVisits / this.attendancy.length) >= 0.9 && (this.getAverageScore() >= 90)) {
+            return "Молодець!";
+        }
+        else if ((sumVisits / this.attendancy.length) >= 0.9 || (this.getAverageScore() >= 90)) {
+            return "Добре, але можна краще";
+        }
+        else {
+            return "Редиска!";
+        }
+    }
 }
 
 
-// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, але на 25 елементів. Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на чергове порожнє місце, в масив записується true, коли викликаємо .absent() - записується false. Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів. Масив – це властивість, present та absent – ​​методи.
+
+const Ivan = new Student('Ivan', 'Shevchenko', 2001, [100, 90, 85, 90, 80, 100]);
+
+Ivan.present();
+Ivan.present();
+Ivan.absent();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.absent();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.absent();
+Ivan.absent();
+Ivan.present();
+Ivan.present();
+Ivan.absent();
+Ivan.absent();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
+Ivan.present();
 
 
 
-// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
+const Maria = new Student('Maria', 'Kononenko', 2005, [10, 90, 45, 60, 30, 10]);
+Maria.absent();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.absent();
+Maria.present();
+Maria.present();
 
 
 
-// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) і показати використання цих методів.
+
+
+console.log("Age: " + Ivan.getAge());
+console.log("Average score: " + Ivan.getAverageScore());
+console.log(Ivan.getVisetedClasses());
+console.log(Ivan.summary());
+
+
+console.log('********************************************');
+
+
+console.log("Age: " + Maria.getAge());
+console.log("Average score: " + Maria.getAverageScore());
+console.log(Maria.getVisetedClasses());
+console.log(Maria.summary());
--- hw24 Гамбургери/script.js
+++ hw24 Гамбургери/script.js
@@ -1,25 +1,18 @@
 // Мережа фастфудів пропонує кілька видів гамбургерів:
-
-class Hamburger{
-  arr = [];
-
-    constructor(){
-        // this.price = 0;
-        // this.calory = 0;
-      
-    }
-}
-const hamb = new Hamburger();
-
-console.log(hamb.arr);
 // маленький (50 тугриків, 20 калорій);
 // великий (100 тугриків, 40 калорій).
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.topping = [];
+  }
 
 // Гамбургер може бути з одним із декількох видів начинок:
-
 // сиром (+ 10 тугриків, + 20 калорій);
 // салатом (+ 20 тугриків, + 5 калорій);
 // картоплею (+ 15 тугриків, + 10 калорій).
+
 
 // Можна додати добавки:
 
@@ -48,3 +41,84 @@ console.log(hamb.arr);
 
 // // А скільки тепер коштує?
 // console.log("Price with sauce: “ + hamburger.calculatePrice());
+
+
+  addTopping(topping) {
+    let isPresent = 0;
+    for (let i = 0; i < this.topping.length; i++) {
+      if (this.topping[i] === topping) {
+        isPresent++;
+      }
+    }
+    if (!isPresent) {
+      this.topping.push(topping);
+    } else {
+      console.log("Такая добавка уже есть");
+    }
+  }
+
+  removeTopping(topping) {
+    for (let i = 0; i < this.topping.length; i++) {
+      if (this.topping[i] === topping) {
+        delete this.topping[i];
+      }
+    }
+  }
+
+  calcPrice() {
+    let totalPrice;
+    let toppingPrice = 0;
+    for (let i = 0; i < this.topping.length; i++) {
+      toppingPrice += this.topping[i].price;
+    }
+    totalPrice = this.size.price + this.stuffing.price + toppingPrice;
+    return totalPrice;
+  }
+
+  calcCalories() {
+    let totalCalories;
+    let toppingPrice = 0;
+    for (let i = 0; i < this.topping.length; i++) {
+      toppingPrice += this.topping[i].calories;
+    }
+    totalCalories = this.size.calories + this.stuffing.calories + toppingPrice;
+    return totalCalories;
+  }
+}
+
+Hamburger.sizeSmall = { name: "Small size", price: 50, calories: 20 };
+Hamburger.sizeBig = { name: "Big size", price: 100, calories: 40 };
+
+Hamburger.stuffingCheese = {
+  name: "stuffing Cheese",
+  price: 10,
+  calories: 20,
+};
+Hamburger.stuffingSalad = { stuff: "stuffing Salad", price: 20, calories: 5 };
+Hamburger.stuffingPotato = {
+  name: "stuffing Potato",
+  price: 15,
+  calories: 10,
+};
+
+Hamburger.toppingMayo = {
+  name: "topping Mayo",
+  price: 20,
+  calories: 5,
+};
+Hamburger.toppingSpice = {
+  name: "topping Spice",
+  price: 15,
+  calories: 0,
+};
+
+const hamburger = new Hamburger(Hamburger.sizeBig, Hamburger.stuffingCheese);
+
+hamburger.addTopping(Hamburger.toppingMayo);
+console.log("Calories: " + hamburger.calcCalories());
+
+console.log("Price: " + hamburger.calcPrice() + " тугриків");
+hamburger.addTopping(Hamburger.toppingSpice);
+console.log("Price with spice: " + hamburger.calcPrice() + " тугриків");
+
+console.log(hamburger);
