b37fb65c3f0c67ef9e1bd28be51958f9cbb7ff59
Eleonora Konzack <elyamd@mail.ru>
2023-06-22 21:54:49 +0200

    hw35

--- hw35 Слайдер базовий/script.js
+++ hw35 Слайдер базовий/script.js
@@ -2,42 +2,42 @@ let slides = document.querySelector(".wrapper");
 const leftButton = document.querySelector('.left-button');
 const rightButton = document.querySelector('.right-button');
 const slidesCount = document.querySelectorAll('.slider-wrapper').length;
-const slides1 = document.querySelectorAll('.slider-wrapper')
+const slides1 = document.querySelectorAll('.slider-wrapper');
 let activeSlideIndex = 0;
 leftButton.style.display = 'none';
 leftButton.addEventListener('click', () => {
-    changeSlide('left')
+    changeSlide('left');
 });
 
 rightButton.addEventListener('click', () => {
-    changeSlide('right')
+    changeSlide('right');
 });
 
 function changeSlide(direction) {
     if (direction === 'right') {
-        activeSlideIndex++
+        activeSlideIndex++;
         if (activeSlideIndex === slidesCount - 1) {
-            rightButton.style.display = 'none'
+            rightButton.style.display = 'none';
         } else {
-            leftButton.style.display = 'block'
+            leftButton.style.display = 'block';
 
         }
 
     } else if (direction === 'left') {
-        activeSlideIndex--
+        activeSlideIndex--;
         if (activeSlideIndex === 0) {
-            leftButton.style.display = 'none'
+            leftButton.style.display = 'none';
         }
         else {
-            rightButton.style.display = 'block'
+            rightButton.style.display = 'block';
         }
     }
 
     for (let i = 0; i < slidesCount; i++) {
         if (i === activeSlideIndex) {
-            slides1[i].style.display = 'flex'
+            slides1[i].style.display = 'flex';
         } else {
-            slides1[i].style.display = 'none'
+            slides1[i].style.display = 'none';
         }
     }
 }
