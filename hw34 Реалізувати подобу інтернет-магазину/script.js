0ca7b3a5af90c74aebf1014b95e0f068d64e1dd4
Eleonora Konzack <elyamd@mail.ru>
2023-07-26 17:09:17 +0200

    hw

--- hw34 Реалізувати подобу інтернет-магазину/script.js
+++ hw34 Реалізувати подобу інтернет-магазину/script.js
@@ -1,5 +1,51 @@
 
-let result = document.querySelector('.result');
-let dod = 78
-result.innerHTML = dod
+
+
+
+
+// const clothes = {
+//     'Мужская': ['Брюки', 'Рубашки', 'Галстуки'],
+//     'Женская': ['Юбки', 'Платья', 'Блузки'],
+//     'Детская': ['Брюки', 'Куртки', 'Перчатки'],
+// };
+
+const categoryElements = document.getElementsByClassName('main-nav').value;
+// const products = document.getElementById('products');
+// const productListElement = document.getElementById('product-list');
+// const productInfo = document.getElementById('product-info');
+// const productDetailsElement = document.getElementById('product-details');
+// const buyButton = document.getElementById('buy-button');
+
+// function showProducts(category) {
+//     productListElement.innerHTML = '';
+//     const products = data[category];
+//         products.forEach((product) => {
+//         const listItem = document.createElement('li');
+//         listItem.innerText = product;
+//         productListElement.appendChild(listItem);
+//     });
+
+// }
+
+// Array.from(categoryElements).forEach((element) => {
+//     element.addEventListener('click', (event) => {
+//         const category = event.target.innerText;
+//         showProducts(category);
+//         productInfo.style.display = 'block';
+//         products.style.display = 'block';
+//     });
+// });
+
+// productListElement.addEventListener('click', (event) => {
+//     const product = event.target.innerText;
+//     productDetailsElement.innerText = 'Ви обрали: ' + product;
+// });
+
+// buyButton.addEventListener('click', ( ) => {
+//     alert('Товар куплений');
+//     productDetailsElement.innerText = '';
+//     productListElement.innerHTML = '';
+//     productInfo.style.display = 'none';
+//     products.style.display = 'none';
+// });
 
